0021bb749bd8a23958771b793d09320dedaec695
zhaobe <zhaoben777@gmail.com>
2017-04-12 00:11:36 -0700

    Updating README and adding test.js, two tests pass.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Simple mocha JS development
 - `npm install request --save`
 - `mkdir test`
 - `touch test/test.js`
+
+## Usage
+- `mocha` runs your test/test.js file and tells if your test blocks pass or not
---
+++ test/test.js
@@ -0,0 +1,30 @@
+var request = require("request"),
+    assert = require("assert"),
+    home_url = "http://localhost:3000/"; //home url at port 3000
+
+describe("Hello World", function() {
+    
+    describe("GET /", function() {
+        
+        //checking status code of 200 success
+        it("returns status 200", function() {
+            request.get(home_url, function(error, response, body) {
+                //expect(response.statusCode).toBe(200);
+                assert.equal(200, response.statusCode);
+                done(); //done callback
+            });
+        });
+        
+        //checking if hello world is in body
+        it("returns Hello World for body", function() {
+            request.get(home_url, function(error, response, body) {
+                 //expect(body).toBe("Hello World");
+                assert.equal("Hello World", body);
+                helloWorld.closeServer();
+                done();
+            });
+        });
+        
+    });
+    
+});
